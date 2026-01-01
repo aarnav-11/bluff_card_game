695c6db83ded2669ead798339b4aa853ca55b4e8
aarnav-11 <aarnav11@g.ucla.edu>
2026-01-01 22:25:42 +0530

    Main distribution logic added

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-
 import random
 
-playerList = []
 class Card:
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
 
-print(playerList)
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
 
 class Player:
-    def __init__ (self, username, cards = []):
+    def __init__(self, username, cards=None):
         self.username = username
-        self.cards = cards
-        playerList.append(self)
+        self.cards = [] if cards is None else cards
 
     def addCard(self, card: Card):
         self.cards.append(card)
 
 Aarnav = Player("Aarnav")
-print(Aarnav.cards)
+Keegan = Player("Keegan")
 
-def dealCards(deck, player):
-    pass
+def dealCards(deck, players, cards_each=5):
+    random.shuffle(deck)
+    for i in range(cards_each):
+        for player in players:
+            player.addCard(deck.pop())
 
-deck = [Card(value, suit) for value in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] for suit in ["hearts", "diamonds", "clubs", "spades"]]
-random.shuffle(deck)
+deck = [Card(value, suit) for value in ["2","3","4","5","6","7","8","9","10","J","Q","K","A"] for suit in ["hearts","diamonds","clubs","spades"]]
 
+if __name__ == "__main__":
+    players = [Aarnav, Keegan]
+    dealCards(deck, players, cards_each=5)
 
-# for card in deck:
-#     print(card.value, "of", card.suit)
+    for p in players:
+        print(p.username, p.cards)
+
+    print("Cards left in deck:", len(deck))
